9f76f56bdc9f85783329eaab7b0aacd8d3dd5329
CH6832 <147909141+CH6832@users.noreply.github.com>
2024-09-28 08:21:12 +0200

    Add files via upload

---
+++ out/build/x64-debug/webpage-opener/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/chris/Desktop/webpage-opener/webpage-opener
+# Build directory: C:/Users/chris/Desktop/webpage-opener/out/build/x64-debug/webpage-opener
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ out/build/x64-release/webpage-opener/CMakeFiles/webpage-opener.dir/resources/resources.i
@@ -0,0 +1,7 @@
+#line 1 "C:\\Users\\chris\\Desktop\\webpage-opener\\webpage-opener\\resources\\resources.rc"
+#line 1 "C:\\Users\\chris\\Desktop\\webpage-opener\\webpage-opener\\resources\\../includes/resources.h"
+
+
+#line 2 "C:\\Users\\chris\\Desktop\\webpage-opener\\webpage-opener\\resources\\resources.rc"
+
+101   RCDATA   "data.xml"
